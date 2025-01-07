0edf3cc5d0708d0c22ebf8259ff0a8a9135418e6
youthgt <youthgt@foxmail.com>
2025-01-07 21:07:11 +0800

    Merge remote-tracking branch 'origin/main'

--- start_train.py
+++ start_train.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     from ultralytics import YOLO
 
     # Load a model
-    model = YOLO('yolov8m-WaveletPool.yaml')  # build a new model from YAML
+    model = YOLO('yolov8.yaml')  # build a new model from YAML
     # model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
     # model = YOLO('path/to/the/YAML').load(
     #              'path/to/the/pre_trained/weights')  # build from YAML and transfer weights
